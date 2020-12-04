7a83a7b21b607dc683eaa7049a2b738f6f31d696
Nicholas Bestauros <nicbest@uw.edu>
2020-12-04 00:28:13 -0800

    Pseudocode from lecture for SearcherImpl

--- src/main/java/edu/uwb/css143b2020fall/service/SearcherImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/SearcherImpl.java
@@ -10,7 +10,20 @@ import java.util.Map;
 public class SearcherImpl implements Searcher {
     public List<Integer> search(String keyPhrase, Map<String, List<List<Integer>>> index) {
         List<Integer> result = new ArrayList<>();
-        // add your code
+        /*
+        split the keyphrase into words by space
+
+        check which documents have all the words from the keyPhrase (rule out the ones that dont have all the given words in keyPHrase)
+
+        find the occurance of the first word of an input in a document, check if the next words are the words we need
+
+        for each common document, get the location index of each word
+
+        determine whether search words are in the correct order
+
+
+        to find intersections, add all unique values to a hashmap
+         */
         return result;
     }
 }
